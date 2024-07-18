7fc8dd959a161dd3a05ebe01b0cafe8117432bbb
Kronight <kronight@outlook.com>
2024-07-19 05:05:45 +0800

    修改配置

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -25,9 +25,9 @@ contract HelperConfig is Script {
     {
         sepoliaNetworkConfig = NetworkConfig({
             subscriptionId: 32257831488387317018826055199802730878101123509143777723426075897625349115262,
-            gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             callbackGasLimit: 500000,
-            vrfCoordinatorV2: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
+            vrfCoordinatorV2: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
             deployerKey: vm.envUint("PRIVATE_KEY")
         });
     }
